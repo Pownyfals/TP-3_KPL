d79e4dc8b604bbe17dfe758943ab6a33f3856add
Kevin Febrian <pownyfals@gmail.com>
2022-03-07 20:58:08 +0700

    menambahkan implementasi dengan tabel-driven

---
+++ tpmodul3_1302204081/tpmodul3_1302204081/KodePos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204081
+{
+	internal class KodePos
+	{
+		private string kelurahan;
+		private int kodePos;
+
+		//constructor
+		public KodePos(string k, int kP)
+		{
+			this.kelurahan = k;
+			this.kodePos = kP;
+		}
+
+		public int getKodePos()
+		{
+			return kodePos;
+		}
+
+	}
+}
--- tpmodul3_1302204081/tpmodul3_1302204081/Program.cs
+++ tpmodul3_1302204081/tpmodul3_1302204081/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tpmodul3_1302204081;
+using static tpmodul3_1302204081.KodePos;
+
+KodePos batununggal = new KodePos("batununggal",40266);
+KodePos kujangsari = new KodePos("kujangsari", 40287);
+KodePos mengger = new KodePos("mengger", 40287);
+KodePos wates = new KodePos("wates", 40256);
+KodePos cijawura = new KodePos("cijawura", 40287);
+KodePos jatisari = new KodePos("jatisari", 40286);
+KodePos margasari = new KodePos("margasari", 40286);
+KodePos sekejati = new KodePos("sekejati", 40286);
+KodePos kebonwaru = new KodePos("kebonwaru", 40272);
+KodePos maleer = new KodePos("maleer", 40274);
+KodePos samoja = new KodePos("samoja", 40273);
+
+
+
